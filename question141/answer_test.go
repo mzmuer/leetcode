d1d9b94b26deaeb1933b1bef7e7cf276f638f40b
mzmuer <mzmuer@gmail.com>
2020-10-09 15:52:59 +0800

    fix add 141

---
+++ question141/answer_test.go
@@ -0,0 +1,51 @@
+// 114. 二叉树展开为链表
+// https://leetcode-cn.com/problems/flatten-binary-tree-to-linked-list/
+
+package question114
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_hasCycle(t *testing.T) {
+	cycle1 := &ListNode{
+		Val: 2,
+		Next: &ListNode{
+			Val: 0,
+			Next: &ListNode{
+				Val: -4,
+			},
+		},
+	}
+	cycle1.Next.Next.Next = cycle1
+	l1 := &ListNode{
+		Val:  3,
+		Next: cycle1,
+	}
+
+	fmt.Println(hasCycle(l1))
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func hasCycle(head *ListNode) bool {
+	if head == nil || head.Next == nil {
+		return false
+	}
+
+	fast := head.Next
+	for fast != head {
+		if fast == nil || fast.Next == nil {
+			return false
+		}
+
+		head = head.Next
+		fast = fast.Next.Next
+	}
+
+	return true
+}
